1f202ccf6fe2a52272dbe4b0a2c0550506c614f4
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2022-08-04 15:30:01 +0600

    First Page

--- src/App.js
+++ src/App.js
@@ -32,10 +32,38 @@ let ppt, registry;
 
 function App() {
 
+  const [courses, setCourses] = useState([{
+      title: "Java Masterclass",
+      description: "Learn Java from scratch",
+      surity: 17,
+      issuer: "",
+      issuer_name: "Tim Buchaka",
+    },
+    {
+      title: "Data Structures & Algorithms",
+      description: "Learn Data Structures & Algorithms",
+      surity: 21,
+      issuer: "",
+      issuer_name: "Tamara Kostova",
+    },
+    {
+      title: "Blockchain",
+      description: "Learn Blockchain",
+      surity: 0,
+      issuer: "",
+      issuer_name: "BUET",
+    }
+  ]);
+
+  const [myCourses, setMyCourse] = useState([]);
+  const [myCertificates, setMyCertificates] = useState([]);
+
   const [account, setAccount] = useState("");
   const [balance, setBalance] = useState("");
   const [properties, setProperties] = useState(null);
   const [purchases, setPurchases] = useState(null);
+
+  const [page, setPage] = useState("courses");
 
   const loadWeb3 = async () => {
     if (typeof window.ethereum !== "undefined") {
@@ -100,7 +128,82 @@ function App() {
       .then(console.log);
   };
 
+  const buyCourse = async (course) => {
+    setCourses(courses.filter(item => item.title !== course.title || item.issuer !== course.issuer));
+    setMyCourse([...myCourses, course]);
+    // TODO : Pay Surity for the course
+  }
+
+  const finishCourse = async (course) => {
+
+  }
+
+  const deadlineReached = async (course) => {
+
+  }
+
+  const extendDeadline = async (course) => {
+
+  }
+
   loadWeb3();
+
+  let content = null;
+
+  if (page === "courses") {
+    content = (
+      <>
+      <Typography variant="h4" gutterBottom component="div" sx={{mb: 4}}>
+        Available Courses
+      </Typography>
+      <Container maxWidth="lg" sx={{mb: 4}}>
+        <Grid container spacing={3}>
+          {courses.map((course, index) => (
+            <Grid item xs={6}>
+              <Item>
+                <Typography variant="h5">{course.title}</Typography>
+                <Typography variant="body1">{course.description}</Typography>
+                <Typography variant="body1">Surity: {course.surity || 'None'}</Typography>
+                <Typography variant="body1" sx={{mb: 2}}>Course By: {course.issuer_name}</Typography>
+
+                <Button variant="contained" color="primary" onClick={() => buyCourse(course)}>Buy</Button>
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+
+      <Typography variant="h4" gutterBottom component="div" sx={{mb: 4}}>
+        My Courses
+      </Typography>
+
+      <Container maxWidth="lg" sx={{mb: 4}}>
+        <Grid container spacing={3}>
+          {myCourses.map((course, index) => (
+            <Grid item xs={6}>
+              <Item>
+                <Typography variant="h5">{course.title}</Typography>
+                <Typography variant="body1">{course.description}</Typography>
+                <Typography variant="body1">Surity: {course.surity || 'None'}</Typography>
+                <Typography variant="body1" sx={{mb: 2}}>Course By: {course.issuer_name}</Typography>
+
+                <Button variant="contained" color="success" size='small' onClick={() => finishCourse(course)}>Finish within deadline</Button>
+                <Button variant="outlined" color="error" size='small' onClick={() => deadlineReached(course)}>Deadline Over</Button>
+                <Button variant="outlined" color="warning" size='small' onClick={() => extendDeadline(course)}>Extend Deadline</Button>
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      </>
+    );
+  } else if (page === 'certificates') {
+    content = <>
+      <Typography variant="h4" gutterBottom component="div" sx={{mb: 4}}>
+        My Certificates
+      </Typography>
+    </>
+  }
 
   return (
     <div className="App">
@@ -109,8 +212,12 @@ function App() {
         <AppBar position="static" style={{ marginBottom: "20px" }}>
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Property DAPP
+              BeeCertify
             </Typography>
+
+            <Button color={"inherit"} variant={page === "courses" && "outlined"} onClick={() => setPage('courses')}>Courses</Button>
+            <Button color={"inherit"} variant={page === "certificates" && "outlined"} onClick={() => setPage('certificates')}>My Certificates</Button>
+            <Button color={"inherit"} variant={page === "verify" && "outlined"} onClick={() => setPage('verify')} sx={{mr: 2}}>Verify</Button>
 
             <p>
               account: { account.substring(0, 20) }... <br/>
@@ -122,10 +229,7 @@ function App() {
 
       <Container maxWidth="md">
 
-        <Typography variant="h4" gutterBottom component="div">
-          Available Properties
-        </Typography>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}
+        {/* <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 3, md: 12 }}
           style={{ marginBottom: "20px" }}>
           {properties && properties.map((item, index) => item.owner !== account && (
             <Grid item xs={2} sm={4} md={4} key={index}>
@@ -143,9 +247,11 @@ function App() {
               </Item>
             </Grid>
           ))}
-        </Grid>
+        </Grid> */}
 
-        <Typography variant="h4" gutterBottom component="div">
+        {content}
+
+        {/* <Typography variant="h4" gutterBottom component="div">
           Owned Properties
         </Typography>
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}
@@ -200,7 +306,7 @@ function App() {
               ))}
             </TableBody>
           </Table>
-        </TableContainer>
+        </TableContainer> */}
       </Container>
       </Box>
     </div>
